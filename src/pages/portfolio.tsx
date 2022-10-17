b755b46c73b08d4a93d366cf5d31f5906a5792cb
Toby Devlin <toby@tobydevlin.com>
2022-10-17 18:44:16 +0100

    add meshd portfolio

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -6,7 +6,7 @@ import {Card, Row} from 'react-bootstrap';
 import {titleAnimation, titleAnimationDuration} from '../helper/constants';
 import SEO from '../components/SEO';
 import {GatsbyImage, getImage, IGatsbyImageData} from 'gatsby-plugin-image';
-import {graphql} from 'gatsby';
+import {graphql, Link} from 'gatsby';
 import {PortfolioDataQuery} from '../../graphql-types';
 import {OutboundLink} from "gatsby-plugin-google-gtag";
 
@@ -15,22 +15,30 @@ interface PortfolioCardProps {
     imgData: IGatsbyImageData;
     imgAlt: string;
     url: string;
+    url_2?: string;
     description?: string;
 }
 
 const SiteCard: FunctionComponent<PortfolioCardProps> = (props) => {
     const imageData = getImage(props.imgData);
     return (
-        <Card className={styles.card}>
-            <Card.Img variant="top" as={GatsbyImage} image={imageData} alt={props.imgAlt}/>
-            <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
-                <Card.Text>{props.description}</Card.Text>
-                <OutboundLink href={props.url}>
-                    Go there
-                </OutboundLink>
-            </Card.Body>
-        </Card>
+        <div className="col-lg-6 col-sm-12">
+            <Card className={`px-0 m-4 ${styles.card}`}>
+                <Card.Img variant="top" as={GatsbyImage} image={imageData} alt={props.imgAlt}/>
+                <Card.Body>
+                    <Card.Title>{props.name}</Card.Title>
+                    <Card.Text>{props.description}</Card.Text>
+                    <OutboundLink href={props.url}>
+                        Go there
+                    </OutboundLink>
+                    {props.url_2 &&
+                        <OutboundLink href={props.url_2}>
+                            API Docs
+                        </OutboundLink>
+                    }
+                </Card.Body>
+            </Card>
+        </div>
     );
 };
 
@@ -40,6 +48,15 @@ interface PortfolioProps {
 
 const Portfolio = (props: PortfolioProps) => {
     let portfolioData: Array<PortfolioCardProps> = [
+        {
+            name: 'MeshD Cloud',
+            imgData: props.data.meshd,
+            imgAlt: 'MeshD homepage',
+            url: 'https://meshd.cloud',
+            url_2: 'https://api.meshd.cloud',
+            description:
+                'A new data service mesh for modern data development. Cloud native and distributed, Meshd is the next iteration of data warehousing for the modern data stack. https://api.meshd.com/redoc'
+        },
         {
             name: 'Chiddingfold Bonfire',
             imgData: props.data.bonfire,
@@ -72,24 +89,29 @@ const Portfolio = (props: PortfolioProps) => {
                 Other Projects I've Created.
             </h1>
             <hr/>
-            {portfolioData.map((data, idx) => (
-                <Row className={styles.cardContainer}>
+            <Row className={`align-items-center ${styles.cardContainer}`}>
+                {portfolioData.map((data, idx) => (
                     <SiteCard
+                        key={idx}
                         name={data.name}
                         imgData={data.imgData}
                         imgAlt={data.imgAlt}
                         url={data.url}
                         description={data.description}
                     />
-                    <br/>
-                </Row>
-            ))}
+                ))}
+            </Row>
         </Content>
     );
 };
 
 export const query = graphql`
     query PortfolioData {
+        meshd: file(base: {eq: "meshd-api-docs.png"}) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP])
+            }
+        }
         bonfire: file(base: {eq: "bonfire-homepage.png"}) {
             childImageSharp {
                 gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP])
