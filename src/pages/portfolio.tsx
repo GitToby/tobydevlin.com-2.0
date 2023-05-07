fed943f05a23ce8e7d896a2e700c125e05143435
Toby Devlin <toby@tobydevlin.com>
2023-05-07 17:02:20 +1000

    update code portfolio with new projects

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -6,9 +6,9 @@ import {Card, Row} from 'react-bootstrap';
 import {titleAnimation, titleAnimationDuration} from '../helper/constants';
 import SEO from '../components/SEO';
 import {GatsbyImage, getImage, IGatsbyImageData} from 'gatsby-plugin-image';
-import {graphql, Link} from 'gatsby';
+import {graphql} from 'gatsby';
 import {PortfolioDataQuery} from '../../graphql-types';
-import {OutboundLink} from "gatsby-plugin-google-gtag";
+import {OutboundLink} from 'gatsby-plugin-google-gtag';
 
 interface PortfolioCardProps {
     name: string;
@@ -23,19 +23,13 @@ const SiteCard: FunctionComponent<PortfolioCardProps> = (props) => {
     const imageData = getImage(props.imgData);
     return (
         <div className="col-lg-6 col-sm-12">
-            <Card className={`px-0 m-4 ${styles.card}`}>
-                <Card.Img variant="top" as={GatsbyImage} image={imageData} alt={props.imgAlt}/>
+            <Card className={`px-0 mx-1 my-2 ${styles.card}`}>
+                <Card.Img variant="top" as={GatsbyImage} image={imageData} alt={props.imgAlt} />
                 <Card.Body>
                     <Card.Title>{props.name}</Card.Title>
                     <Card.Text>{props.description}</Card.Text>
-                    <OutboundLink href={props.url}>
-                        Go there
-                    </OutboundLink>
-                    {props.url_2 &&
-                        <OutboundLink href={props.url_2}>
-                            API Docs
-                        </OutboundLink>
-                    }
+                    <OutboundLink href={props.url}>Go there</OutboundLink>
+                    {props.url_2 && <OutboundLink href={props.url_2}>API Docs</OutboundLink>}
                 </Card.Body>
             </Card>
         </div>
@@ -48,6 +42,15 @@ interface PortfolioProps {
 
 const Portfolio = (props: PortfolioProps) => {
     let portfolioData: Array<PortfolioCardProps> = [
+        {
+            name: 'Framelink',
+            imgData: props.data.framelink,
+            imgAlt: 'Framelink package homepage',
+            url: 'https://pypi.org/project/framelink/',
+            url_2: 'https://github.com/GitToby/framelink/',
+            description:
+                "Framelink is a simple wrapper that's designed to provide context into pandas, polars and other Dataframe engines. It should provide a way for collaborating teams to write python or SQL models to see their data flow easily and get the a whole load of stuff for free!"
+        },
         {
             name: 'MeshD Cloud',
             imgData: props.data.meshd,
@@ -64,6 +67,24 @@ const Portfolio = (props: PortfolioProps) => {
             url: 'https://chiddingfoldbonfire.org.uk',
             description:
                 'The local village event of the year is that English tradition of burning a terrorist at the stake. And in the 21st century that requires a website so people know where and when.'
+        },
+        {
+            name: 'scribr',
+            imgData: props.data.scribr,
+            imgAlt: 'Framelink homepage',
+            url: 'https://pypi.org/project/framelink/',
+            url_2: 'https://github.com/GitToby/framelink/',
+            description:
+                'scribr is all-in-one note-taking tool that allows you to effortlessly jot down notes in your cli, and securely save them to a file.'
+        },
+        {
+            name: 'Angry Logging',
+            imgData: props.data.angryLogger,
+            imgAlt: 'Angry logging homepage',
+            url: 'https://pypi.org/project/angry-logger/',
+            url_2: 'https://github.com/GitToby/angry-logger',
+            description:
+                'Do you want to show your logger to be more passive aggressive? maybe just actual aggressive? This is the library for you.'
         },
         {
             name: 'Zoom A Chicken Live',
@@ -84,11 +105,11 @@ const Portfolio = (props: PortfolioProps) => {
     ];
     return (
         <Content>
-            <SEO pageTitle="Portfolio" isBlogPost={false}/>
+            <SEO pageTitle="Portfolio" isBlogPost={false} />
             <h1 data-aos={titleAnimation} data-aos-duration={titleAnimationDuration} data-aos-delay="0">
                 Other Projects I've Created.
             </h1>
-            <hr/>
+            <hr />
             <Row className={`align-items-center ${styles.cardContainer}`}>
                 {portfolioData.map((data, idx) => (
                     <SiteCard
@@ -107,7 +128,17 @@ const Portfolio = (props: PortfolioProps) => {
 
 export const query = graphql`
     query PortfolioData {
-        meshd: file(base: {eq: "meshd-api-docs.png"}) {
+        meshd: file(base: {eq: "meshd-home.png"}) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP])
+            }
+        }
+        framelink: file(base: {eq: "framelink.png"}) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP])
+            }
+        }
+        scribr: file(base: {eq: "scribr.png"}) {
             childImageSharp {
                 gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP])
             }
@@ -118,6 +149,11 @@ export const query = graphql`
             }
         }
         zoomChicken: file(base: {eq: "zoom-a-chicken-homepage.png"}) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP])
+            }
+        }
+        angryLogger: file(base: {eq: "angry-logger.png"}) {
             childImageSharp {
                 gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP])
             }
